cef92fb0cf99665c916c5306e5959f45bd49aa0e
bee2 <o.lountain@gmail.com>
2018-05-30 20:57:54 +0930

    part b model fitting

--- PartB.R
+++ PartB.R
@@ -1,26 +1,30 @@
 setwd("C:/Users/Oliver/OneDrive/Documents/2018/Semester 1/Statistical Modelling/Project")
-mammo <- read.table("mammo.txt", header = T, sep = ',')
-# mammo<-read.csv("mammo.txt",header=TRUE,na.strings = "?") reading the data this way eliminates lines 10-16
+
 library(broom)
 library(MASS)
+library(magrittr)
+library(dplyr)
 
 
 # ---- Data Entry and Cleaning ----
-#removing '?' values:
-table(mammo)
-mammo$BI.RADS[mammo$BI.RADS == "?"] <- NA
-mammo$Age[mammo$Age == "?"] <- NA
-mammo$Shape[mammo$Shape == '?'] <- NA
-mammo$Margin[mammo$Margin == "?"] <- NA
-mammo$Density[mammo$Density == "?"] <- NA
-mammo$Severity[mammo$Severity == "?"] <- NA
+#reading data and removing '?' values:
+mammo <- read.csv("mammo.txt", header=TRUE, na.strings = "?")
+mammo <- dplyr::select(mammo, Age, Shape, Margin, Density, Severity)
+
+
+#checking variable types and converting
+str(mammo)
+mammo$Shape <- as.factor(mammo$Shape)
+mammo$Margin <- as.factor(mammo$Margin)
+mammo$Density <- as.factor(mammo$Density)
+mammo$Severity <- as.factor(mammo$Severity)
+
 
 
 # ---- Data Visualisation and Summaries ----
 pairs(mammo)
 
-corMat <- cor(mammo)
-round(corMat, 4)
+summary(mammo)
 
 summary(mammo$BI.RADS)
 summary(mammo$Age)
@@ -31,8 +35,31 @@ summary(mammo$Severity)
 
 
 # ---- Model Fitting and Selection ----
-M1 <- lm(Severity ~ BI.RADS+Age+Shape+Margin+Density, data = mammo) #cant use lm here because you have binanry response
-tidy(M1) 
+M1 <- glm(Severity ~ Age*Shape*Margin*Density, data = mammo, family = "binomial")
+tidy(M1)
 summary(M1)
-S2 <- 0.3471^2
-step(M1, scale = S2)
+
+
+# Age, Shape and Margin are the only statistically significant predictors
+
+M2 <- glm(Severity ~ Age + Shape + Margin, data = mammo, family = "binomial")
+summary(M2)
+
+M3 <- glm(Severity ~ (Age + Shape + Margin + Density)^2, data = mammo, family = "binomial")
+summary(M3)
+
+summary(glm(Severity ~ (Age + Shape + Margin + Density)^2, data = mammo, family = "binomial"))
+
+
+M4 <- glm(Severity ~ Age + Shape + Margin + Age + Age : Shape + Age : Margin, data = mammo, family = "binomial")
+summary(M4)
+
+T1 <- glm(Severity ~ (Age + Shape + Margin + Density)^2, data = mammo, family = "binomial")
+summary(T1)
+
+back.glm <- T1
+
+drop1(back.glm, test = "F")
+back.glm <- update(back.glm, .~. - Margin:Density)
+
+drop1(back.glm, test = "F")
